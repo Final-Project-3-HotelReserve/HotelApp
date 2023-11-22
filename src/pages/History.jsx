dd1f939d3c88026153e20d6a2b480cb219ed0097
bagasrnf <bagasrnf2002@gmail.com>
2023-11-23 00:02:32 +0700

    add setting/profile screen

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,12 +1,12 @@
 import { Text, View } from "react-native";
 import React from "react";
 
-const Setting = () => {
+const History = () => {
   return (
     <View>
-      <Text>Setting Page</Text>
+      <Text>History Page</Text>
     </View>
   );
 };
 
-export default Setting;
+export default History;
